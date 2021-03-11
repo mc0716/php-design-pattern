97e957a58eb46ef0c928f7a0f941ab49e4d0df15
马福超 <84106749@qq.com>
2021-03-11 15:45:15 +0800

    单例模式

---
+++ 创建型/单例模式（Singleton）/Test01/Singleton.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Singleton\Test01;
+
+
+final class Singleton
+{
+    private static $instance;
+
+    private $array = array(1, 2, 3, 4, 5);
+
+    /**
+     * 通过懒加载获得实例（在第一次使用的时候创建）
+     */
+    public static function getInstance(): Singleton
+    {
+        if (null === static::$instance) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * 不允许从外部调用以防止创建多个实例
+     * 要使用单例，必须通过 Singleton::getInstance() 方法获取实例
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * 防止实例被克隆（这会创建实例的副本）
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * 防止反序列化（这将创建它的副本）
+     */
+    private function __wakeup()
+    {
+    }
+
+    public function print_sum()
+    {
+        print(array_sum($this->array));
+    }
+}
+
+$object = Singleton::getInstance();
+$object->print_sum();
